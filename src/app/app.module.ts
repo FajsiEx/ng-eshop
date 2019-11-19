22f197e71e42bda2d42b14e3dbc3e7270562c61a
Cody Maverick <bobkomatej1@gmail.com>
2019-11-19 15:56:25 +0100

    +section-quality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shell/navbar/navbar.component';
 import { FooterComponent } from './shell/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SectionQualityComponent } from './pages/home/section-quality/section-quality.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    SectionQualityComponent
   ],
   imports: [
     BrowserModule,
