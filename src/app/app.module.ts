9bf57362d3ddc95a9f34d57ac738b683076b40e6
Patrik Sith <patrik.sith@gmail.com>
2019-11-24 16:14:10 +0100

    section hero update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shell/navbar/navbar.component';
 import { FooterComponent } from './shell/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SectionHeroComponent } from './pages/home/section-hero/section-hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    SectionHeroComponent
   ],
   imports: [
     BrowserModule,
