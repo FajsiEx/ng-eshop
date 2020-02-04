e5fc4f593fd8de7fd43a5b5e43e9febb40c50c29
TroliaKefa <patrik.sith@gmail.com>
2020-02-04 15:41:22 +0100

    add card components, add service

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+  data: any = [];
 
-  constructor() { }
+  constructor(private api: ApiService) {
+    this.fetchData();
+  }
 
   ngOnInit() {
+  }
+
+  fetchData() {
+    this.api.getProducts().subscribe(data => {
+      this.data = data;
+    })
   }
 
 }
